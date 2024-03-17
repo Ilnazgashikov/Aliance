453d1ca35c230d3864cf653eb928a2e09c5a45a6
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-03-17 18:10:04 +0300

    add: navbar fix

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector(".nav-bar");
+window.addEventListener('scroll', ()=>{
+    if (this.scrollY > 1) {
+        navbar.classList.add("nav-bar-light");
+        document.getElementById("logo-image").src="../img/logo-black.svg";
+
+    }
+    else {
+        navbar.classList.remove("nav-bar-light");
+        document.getElementById("logo-image").src="../img/logo.svg";
+    }
+})
