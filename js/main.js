6c01b693c39725ff43cee8ac1b3a4f7321703787
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-03-21 20:31:19 +0300

    pixel-perfect

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', ()=>{
     if (this.scrollY > 1) {
         navbar.classList.add("nav-bar-light");
         document.getElementById("logo-image").src="../img/logo-black.svg";
-
+        
     }
     else {
         navbar.classList.remove("nav-bar-light");
