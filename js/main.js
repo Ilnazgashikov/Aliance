bdc59c660667dba58eb5ac729b3a04c500e92757
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-03-23 22:34:35 +0300

    add: sprite icons, add mobile-menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 const navbar = document.querySelector(".nav-bar");
+const logo = document.querySelector(".logo-image use");
+const menutogle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+
 window.addEventListener('scroll', ()=>{
     if (this.scrollY > 1) {
         navbar.classList.add("nav-bar-light");
-        document.getElementById("logo-image").src="../img/logo-black.svg";
-        
+        logo.href.baseVal = "img/sprites.svg#logo-black";
     }
     else {
         navbar.classList.remove("nav-bar-light");
-        document.getElementById("logo-image").src="../img/logo.svg";
+        logo.href.baseVal = "img/sprites.svg#logo";
     }
-})
+});
+menutogle.addEventListener("click", (event) => {
+    event.preventDefault();
+    menu.classList.toggle("is-open");
+
+});
+
