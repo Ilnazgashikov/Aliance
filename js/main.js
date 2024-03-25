a8ad9e1910a59de8ecc374ceff5f58d8b32d1e8d
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-03-26 00:22:39 +0300

    add: slider for header

--- js/main.js
+++ js/main.js
@@ -1,21 +1,59 @@
 const navbar = document.querySelector(".nav-bar");
 const logo = document.querySelector(".logo-image use");
-const menutogle = document.querySelector(".mobile-menu-toggle");
+const menuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    autoHeight: false,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        576: {
+          slidesPerView: 2,
 
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 3,
+        },
+        // when window width is >= 640px
+        1024: {
+          slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+          },
+    }
+  });
+const lightMod = (event) => {
+    navbar.classList.add("nav-bar-light");
+    logo.href.baseVal = "img/sprites.svg#logo-black";
+} 
+const blackMod = (event) => {
+    navbar.classList.remove("nav-bar-light");
+    logo.href.baseVal = "img/sprites.svg#logo";
+} 
+const openMenu = (event) => {
+    menu.classList.add("is-open");
+    menuToggle.classList.add("close-menu");
+    document.body.style.overflow = "hidden";
+    lightMod();
+}
+const closeMenu = (event) => {
+    menu.classList.remove("is-open");
+    menuToggle.classList.remove("close-menu");
+    document.body.style.overflow = "";
+    blackMod();
+}
 window.addEventListener('scroll', ()=>{
-    if (this.scrollY > 1) {
-        navbar.classList.add("nav-bar-light");
-        logo.href.baseVal = "img/sprites.svg#logo-black";
-    }
-    else {
-        navbar.classList.remove("nav-bar-light");
-        logo.href.baseVal = "img/sprites.svg#logo";
-    }
+    this.scrollY > 1? lightMod() : blackMod();
 });
-menutogle.addEventListener("click", (event) => {
+    menuToggle.addEventListener("click", (event) => {
     event.preventDefault();
-    menu.classList.toggle("is-open");
-
+    menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
 
