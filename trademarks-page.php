64d06ba68a77911f879c14bb76800541bd47fc3c
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-05-05 22:30:17 +0300

    fix: trademark section

--- trademarks-page.php
+++ trademarks-page.php
@@ -4,4 +4,4 @@ $header_style = "first-header-state";
 $img_src = "";
 include_once('header-page.php'); ?>
 <?php include_once('template-parts/trademarks-section.php'); ?>
-<?php include_once('footer.php'); ?>
+<?php include_once('template-parts/footer.php'); ?>
